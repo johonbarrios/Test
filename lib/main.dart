0780aad1c16f7439a73c8c4e1967f89fb3b8cc7b
moradoraxi <arrebol6013@gmail.com>
2022-03-02 15:54:13 -0500

    Proyecto Inicial

---
+++ lib/main.dart
@@ -0,0 +1,192 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:test/add.dart';
+import 'package:test/database.dart';
+import 'package:test/view.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  //MyHomePage({required Key key, required this.title}) : super(key: key);
+
+  late final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Database db;
+  List docs = [];
+  initialise() {
+    db = Database();
+    db.initiliase();
+    db.read().then((value) => {
+          setState(() {
+            docs = value;
+          })
+        });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialise();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(56, 75, 49, 1.0),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(56, 75, 49, 1.0),
+        title: Text("Country"),
+      ),
+      body: ListView.builder(
+        itemCount: docs.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            margin: EdgeInsets.all(10),
+            child: ListTile(
+              onTap: () {
+                Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                View(country: docs[index], db: db)))
+                    .then((value) => {
+                          if (value != null) {initialise()}
+                        });
+              },
+              contentPadding: EdgeInsets.only(right: 30, left: 36),
+              title: Text(docs[index]['name']),
+              trailing: Text(docs[index]['code']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Add(
+                        db: db,
+                      )));
+          /*Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Add(db: db, key: null,)))
+              .then((value) {
+            if (value != null) {
+              initialise();
+            }
+          });*/
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+
+
+
+/*
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp().then((value) {
+    runApp(MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List usuarios = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getUsers();
+  }
+
+  void getUsers() async {
+    CollectionReference collectionReference =
+        FirebaseFirestore.instance.collection("users");
+    QuerySnapshot users = await collectionReference.get();
+
+    if (users.docs.length != 0) {
+      for (var doc in users.docs) {
+        print(doc.data());
+        usuarios.add(doc.data());
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '3',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+*/
